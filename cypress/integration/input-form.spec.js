b6419d29523a1b67426fa5d84e8e3683dbcaf266
Katie Duane <katiejduane@gmail.com>
2019-06-14 15:50:34 -0400

    form submission tests complete

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,3 +1,5 @@
+import { isContext } from "vm";
+
 describe('Input form', () => {
     beforeEach(()=> {
         cy.visit('/')
@@ -15,4 +17,47 @@ describe('Input form', () => {
         .type(typed)
         .should('have.value', typed)
     })
+    
+    context('Form Submission', () => {
+        beforeEach(() => {
+            cy.server()
+        })
+        it('Adds a new todo on submit', () => {
+            const itemText = 'Buy eggs'
+            cy.route('POST', '/api/todos', {
+                name: itemText,
+                id: 1,
+                isComplete: false
+            })
+            cy.get('.new-todo')
+                .type(itemText)
+                .type('{enter}')
+                .should('have.value', '')
+
+            // in curly brackets, a key press is denoted
+            cy.get('.todo-list li')
+                .should('have.length', 1)
+                .and('contain', itemText)
+                // and is like should but makes the test read more like a sentence
+        })
+
+        it('Shows an error message on a dailed submission', () => {
+            cy.route({
+                url: '/api/todos',
+                method: 'POST',
+                status: 500,
+                response: {}
+            })
+
+            cy.get('.new-todo')
+                .type('test{enter}')
+
+            cy.get('.todo-list li')
+                .should('not.exist')
+            
+            cy.get('.error')
+                .should('be.visible')
+        })
+    });
 })
+
